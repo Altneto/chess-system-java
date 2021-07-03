5d1bfea97357f17998e290aa4407569900814a0e
Altneto <neto1171995@gmail.com>
2021-07-03 14:31:33 -0300

    Criação da classe position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int row = 0;
+        int column = 0;
+        Position position = new Position(row, column);
+        System.out.println(position);
     }
 }
