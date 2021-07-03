ad0ffe8c231728eeb1d8e5a5b49e69bc52a42098
Altneto <neto1171995@gmail.com>
2021-07-03 15:58:48 -0300

    Tabuleiro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,12 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
     public static void main(String[] args) {
-        int row = 0;
-        int column = 0;
-        Position position = new Position(row, column);
-        System.out.println(position);
+        ChessMatch chessMatch = new ChessMatch();
+        UI.printBoard(chessMatch.getPieces());
     }
 }
